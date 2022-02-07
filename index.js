5e57d16aea3612521a5a3ed8ae2942c60bad7ba6
Sumer Patel <patelsum@oregonstate.edu>
2022-02-06 23:46:28 -0800

    Swagger b (#4)
    * secured v1/hello & v1/weather
    
    * removed some lines
    
    * secured /v1/hello && /v1/weather
    
    * updated with ec2 instance ip address

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ console.log('Node-Express server is running on port 3000');
 
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    const allowedOrigins = ['https://editor.swagger.io', 'https://hoppscotch.io'];
+    const allowedOrigins = ['https://editor.swagger.io', 'https://hoppscotch.io', 'http://50.16.127.52:3000', 'http://ec2-50-16-127-52.compute-1.amazonaws.com:3000'];
     const origin = req.headers.origin;
     
     if (allowedOrigins.includes(origin)) {
